3a91b090ae88353bcf2c24a6f44018fe10b90db2
JCTHUM1991 <ryofu1991@gmail.com>
2025-06-07 01:12:50 +0800

    cor

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const fetch = require("node-fetch");
 const app = express();
+
+app.use(cors({
+  origin: "https://jcthum1991.github.io" // or "*" to allow all (not recommended in production)
+}));
+
 
 app.use(express.static("public"));
 app.use(express.json());
